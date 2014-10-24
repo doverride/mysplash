3ff01c1f83a1347566ddb57c5cfc0c75eb0d3de4
Dominick Grift <dac.override@gmail.com>
2014-10-24 09:02:32 +0200

    Deal with modules management differently
    It is a crappy implementation, i know
    
    The idea is that people might want a custom modules configuration and so if LISTING exists then secilc will just try to use it
    This allows one to add modules to the list and remove from the list manually
    
    If LISTING does not exist then splash.sh generates one that includes all .cil suffixed files except the ones in "EXCLUDE" layers (currently the testing layer only)
    
    Furthermore, splash.sh accepts all options that secilc accepts. Thus for example: "./splash.sh -D" would build policy without dontaudit rules

--- splash.sh
+++ splash.sh
@@ -1,46 +1,23 @@
 #!/bin/sh
 
-test -d policy || mkdir policy
+test -d policy || /bin/mkdir policy
 
-contrib ()
+EXCLUDE="tests"
+
+gen_listing ()
 
 {
+    local MODULES=$(/bin/find . -type f \( -iname "*.cil" \) | /bin/cut -d/ -f2-)
 
-LAYERS+=($(for a in $(ls source/contrib) ; do printf "%s\n" "source/contrib/$a" ; done))
-
-for LAYER in ${LAYERS[*]} ; do
-        MODULES+=($(for b in ${LAYERS[*]} ; do printf "%s\n" "$(ls $b | grep .*\.cil$)" ; done))
-
-        for MODULE in ${MODULES[*]} ; do
-                printf "%s\n" "$LAYER/$MODULE"
+    for i in $EXCLUDE ;
+        do
+            MODULES=$(printf "%s\n" "$MODULES" | /bin/grep -v $i)
         done
-done
 
+    printf "%s\n" "$MODULES" | /bin/sort > LISTING
 }
 
 /usr/bin/secilc \
-    source/av.cil \
-    source/base/dev.cil \
-    source/base/file.cil \
-    source/base/fs.cil \
-    source/base/net.cil \
-    source/base/sec.cil \
-    source/base/stor.cil \
-    source/base/sys.cil \
-    source/base/term.cil \
-    source/base/unconfined.cil \
-    source/config.cil \
-    source/constraints.cil \
-    source/entity.cil \
-    source/isid.cil \
-    source/mls.cil \
-    source/object.cil \
-    source/polcap.cil \
-    source/subject.cil \
-    source/support/classperms.cil \
-    source/support/classmaps.cil \
-    source/support/filepatterns.cil \
-    source/support/ipcpatterns.cil \
-    source/support/miscpatterns.cil \
-    $(contrib) \
-   -M false -o policy/policy.29 -f contexts/files/file_contexts
+    $(test -f LISTING || gen_listing && /bin/cat LISTING) \
+    $* \
+    -o policy/policy.29 -f contexts/files/file_contexts
